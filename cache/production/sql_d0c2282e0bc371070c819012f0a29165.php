eca498100b15e0bbc50ea90f531664bb0a23829b
hungnd1 <hungnd1@vivas.vn>
2017-05-06 14:18:16 +0700

    a

--- cache/production/sql_d0c2282e0bc371070c819012f0a29165.php
+++
@@ -1,5 +0,0 @@
-<?php exit; ?>
-1494045061
-SELECT forum_id, forum_name, parent_id, forum_type, forum_flags, forum_options, left_id, right_id FROM phpbb_forums ORDER BY left_id ASC
-905
-a:4:{i:0;a:8:{s:8:"forum_id";s:1:"3";s:10:"forum_name";s:9:"category1";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"0";s:11:"forum_flags";s:2:"32";s:13:"forum_options";s:1:"0";s:7:"left_id";s:1:"1";s:8:"right_id";s:1:"4";}i:1;a:8:{s:8:"forum_id";s:1:"4";s:10:"forum_name";s:12:"Category con";s:9:"parent_id";s:1:"3";s:10:"forum_type";s:1:"1";s:11:"forum_flags";s:2:"48";s:13:"forum_options";s:1:"0";s:7:"left_id";s:1:"2";s:8:"right_id";s:1:"3";}i:2;a:8:{s:8:"forum_id";s:1:"1";s:10:"forum_name";s:19:"Your first category";s:9:"parent_id";s:1:"0";s:10:"forum_type";s:1:"0";s:11:"forum_flags";s:2:"32";s:13:"forum_options";s:1:"0";s:7:"left_id";s:1:"5";s:8:"right_id";s:1:"8";}i:3;a:8:{s:8:"forum_id";s:1:"2";s:10:"forum_name";s:16:"Your first forum";s:9:"parent_id";s:1:"1";s:10:"forum_type";s:1:"1";s:11:"forum_flags";s:2:"48";s:13:"forum_options";s:1:"0";s:7:"left_id";s:1:"6";s:8:"right_id";s:1:"7";}}
